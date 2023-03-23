8fcbb43f639c27b5b769f329bffef003e77e4e68
PeterNassrat <peternassrat90@gmail.com>
2023-03-23 16:22:38 +0200

    puting manual remove tests

--- DatabaseTest.java
+++ DatabaseTest.java
@@ -3,8 +3,19 @@ import org.junit.jupiter.api.Test;
 class DatabaseTest {
 
 	Database data = new Database();
+
 	@Test
-	void test() {
+	void removeTest01() {
+		//ToDo
+	}
+
+	@Test
+	void removeTest02() {
+		//ToDo
+	}
+
+	@Test
+	void removeTest03() {
 		//ToDo
 	}
 
